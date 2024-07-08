60bc0f04b169c4df0536dce6d4ab3ee7b0214ee8
Pavan P <110079444+C-S23@users.noreply.github.com>
2024-07-08 20:08:31 +0530

    Delete EXAMPLE.php

--- EXAMPLE.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("DATABASE_LOCAL", "localhost");
-define("DATABASE_NAME", "Artstore");
-define("DATABASE_USER", "fred");
-define("DATABASE_PASSWD", "F5^7d");
-$db = new mysqli(DATABASE_LOCAL,DATABASE_PASSWD,DATABASE_NAME,DATABASE_USER);
-?>
